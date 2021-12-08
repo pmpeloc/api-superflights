d66a524f639feef6f391ab534372622c785ff2ec
Pablo Misael Peloc <pmpeloc@gmail.com>
2021-12-08 17:49:43 -0300

    Modulo Passenger

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { PassengerModule } from './passenger/passenger.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { UserModule } from './user/user.module';
     }),
     MongooseModule.forRoot(process.env.URI_MONGODB),
     UserModule,
+    PassengerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
