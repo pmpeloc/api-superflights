7d40f202c45034b582d1558fe5b71bdd768d0152
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-02-06 10:41:26 -0300

    feat: Schema flight added

--- src/flight/flight.module.ts
+++ src/flight/flight.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FLIGHT } from 'src/common/models/models';
 import { FlightController } from './flight.controller';
 import { FlightService } from './flight.service';
+import { FlightSchema } from './schema/flight.schema';
+import { PassengerModule } from '../passenger/passenger.module';
 
 @Module({
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: FLIGHT.name,
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        useFactory: () => FlightSchema.plugin(require('mongoose-autopopulate')),
+      },
+    ]),
+    PassengerModule,
+  ],
   controllers: [FlightController],
-  providers: [FlightService]
+  providers: [FlightService],
 })
 export class FlightModule {}
