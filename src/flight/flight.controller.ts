877537067653f743ace3bfc289fece36a6dbf5b5
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-02-06 11:01:31 -0300

    feat: POST new flight added

--- src/flight/flight.controller.ts
+++ src/flight/flight.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { FlightDTO } from './dto/flight.dto';
+import { FlightService } from './flight.service';
 
-@Controller('flight')
-export class FlightController {}
+@Controller('api/v1/flight')
+export class FlightController {
+  constructor(private readonly flightService: FlightService) {}
+
+  @Post()
+  create(@Body() flightDTO: FlightDTO) {
+    return this.flightService.create(flightDTO);
+  }
+}
--- src/flight/flight.service.ts
+++ src/flight/flight.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { IFlight } from 'src/common/interfaces/flight.interface';
+import { FLIGHT } from 'src/common/models/models';
+import { FlightDTO } from './dto/flight.dto';
 
 @Injectable()
-export class FlightService {}
+export class FlightService {
+  constructor(
+    @InjectModel(FLIGHT.name) private readonly model: Model<IFlight>,
+  ) {}
+
+  async create(flightDTO: FlightDTO): Promise<IFlight> {
+    const newFlight = new this.model(flightDTO);
+    return await newFlight.save();
+  }
+}
